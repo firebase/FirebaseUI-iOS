0c6037823d5f90f39e5cd3494c53245af7dd046c
Mike McDonald <mpmcdonald@google.com>
2015-08-24 09:25:29 -0700

    Changing to FirebaseUI Firebase

--- examples/FirebaseUIChat/FirebaseUIChat/ViewController.m
+++ examples/FirebaseUIChat/FirebaseUIChat/ViewController.m
@@ -2,7 +2,6 @@
 //  ViewController.m
 //  FirebaseUIChat
 //
-//  Created by Mike Mcdonald on 8/20/15.
 //  Copyright (c) 2015 Firebase, Inc. All rights reserved.
 //
 
@@ -18,52 +17,65 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-        
-    self.ref = [[Firebase alloc] initWithUrl:@"https://nanochat.firebaseio.com"];
-        
-    self.dataSource = [[MessageDataSource alloc] initWithRef:self.ref modelClass:[Message class] nibNamed:@"MessageTableViewCell" cellReuseIdentifier:@"cellReuseIdentifier" view:self.tableView];
-    
-    [self.dataSource populateCellWithBlock:^void(MessageTableViewCell * __nonnull cell, Message * __nonnull message) {
+  [super viewDidLoad];
+
+  self.ref =
+      [[Firebase alloc] initWithUrl:@"https://firebaseui.firebaseio.com/chat"];
+
+  self.dataSource =
+      [[MessageDataSource alloc] initWithRef:self.ref
+                                  modelClass:[Message class]
+                                    nibNamed:@"MessageTableViewCell"
+                         cellReuseIdentifier:@"cellReuseIdentifier"
+                                        view:self.tableView];
+
+  [self.dataSource
+      populateCellWithBlock:^void(MessageTableViewCell *__nonnull cell,
+                                  Message *__nonnull message) {
         if ([message.name isEqualToString:@"iOS User"]) {
-            cell.myMessageLabel.text = message.message;
-            cell.myNameLabel.text = message.name;
-            cell.myNameLabel.textColor = [UIColor colorWithRed:52.0/255.0 green:170.0/255.0 blue:220.0/255.0 alpha:1.0];
-            [cell.otherMessageLabel setHidden:YES];
-            [cell.otherNameLabel setHidden:YES];
-            [cell.myMessageLabel setHidden:NO];
-            [cell.myNameLabel setHidden:NO];
+          cell.myMessageLabel.text = message.message;
+          cell.myNameLabel.text = message.name;
+          cell.myNameLabel.textColor = [UIColor colorWithRed:52.0 / 255.0
+                                                       green:170.0 / 255.0
+                                                        blue:220.0 / 255.0
+                                                       alpha:1.0];
+          [cell.otherMessageLabel setHidden:YES];
+          [cell.otherNameLabel setHidden:YES];
+          [cell.myMessageLabel setHidden:NO];
+          [cell.myNameLabel setHidden:NO];
         } else {
-            cell.otherMessageLabel.text = message.message;
-            cell.otherNameLabel.text = message.name;
-            cell.otherNameLabel.textColor = [UIColor colorWithRed:164.0/255.0 green:199.0/255.0 blue:57.0/255.0 alpha:1.0];
-            [cell.otherMessageLabel setHidden:NO];
-            [cell.otherNameLabel setHidden:NO];
-            [cell.myMessageLabel setHidden:YES];
-            [cell.myNameLabel setHidden:YES];
+          cell.otherMessageLabel.text = message.message;
+          cell.otherNameLabel.text = message.name;
+          cell.otherNameLabel.textColor = [UIColor colorWithRed:164.0 / 255.0
+                                                          green:199.0 / 255.0
+                                                           blue:57.0 / 255.0
+                                                          alpha:1.0];
+          [cell.otherMessageLabel setHidden:NO];
+          [cell.otherNameLabel setHidden:NO];
+          [cell.myMessageLabel setHidden:YES];
+          [cell.myNameLabel setHidden:YES];
         }
-    }];
-    
-    self.tableView.dataSource = self.dataSource;
-    self.tableView.delegate = self;
+      }];
+
+  self.tableView.dataSource = self.dataSource;
+  self.tableView.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
+  [super didReceiveMemoryWarning];
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
-{
-    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
-}
+- (void)tableView:(UITableView *)tableView
+    didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
+{ [self.tableView deselectRowAtIndexPath:indexPath animated:YES]; }
 
--(BOOL)textFieldShouldReturn:(UITextField *)textField;
+- (BOOL)textFieldShouldReturn:(UITextField *)textField;
 {
-    [[self.ref childByAutoId] setValue:@{@"name": @"iOS User", @"message": textField.text}];
-    [textField resignFirstResponder];
-    textField.text = @"";
-    return YES;
+  [[self.ref childByAutoId]
+      setValue:@{@"name" : @"iOS User", @"message" : textField.text}];
+  [textField resignFirstResponder];
+  textField.text = @"";
+  return YES;
 }
 
 @end
