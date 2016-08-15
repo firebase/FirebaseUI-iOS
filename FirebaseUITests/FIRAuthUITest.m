2d4a3a0073fe29cb3bb1d38c1b86738c1bb64f11
Morgan Chen <morganchen12@gmail.com>
2016-08-15 11:58:40 -0700

    initial FIRAuthUI tests

---
+++ FirebaseUITests/FIRAuthUITest.m
@@ -0,0 +1,73 @@
+//
+//  FIRAuthUITest.m
+//  FirebaseUI
+//
+//  Created by Morgan Chen on 8/15/16.
+//  Copyright © 2016 Firebase, Inc. All rights reserved.
+//
+
+@import XCTest;
+@import Firebase;
+@import FirebaseAuth;
+
+#import "FIRAuthUI.h"
+#import "FIRAuthUIUtils.h"
+#import "FIRAuthPickerViewController.h"
+
+@interface FUIAuthUIDelegate : NSObject<FIRAuthUIDelegate>
+@end
+
+@implementation FUIAuthUIDelegate
+- (void)authUI:(FIRAuthUI *)authUI didSignInWithUser:(FIRUser *)user error:(NSError *)error {
+}
+
+- (FIRAuthPickerViewController *)authPickerViewControllerForAuthUI:(FIRAuthUI *)authUI {
+  Class controllerClass = [FIRAuthPickerViewController class];
+  NSString *classString = NSStringFromClass(controllerClass);
+  NSBundle *bundle = [NSBundle bundleForClass:controllerClass];
+  return [[FIRAuthPickerViewController alloc] initWithNibName:classString
+                                                             bundle:bundle
+                                                             authUI:authUI];
+}
+@end
+
+@interface FIRAuthUITest : XCTestCase
+@property (nonatomic) FIRAuth *auth;
+@property (nonatomic) FIRAuthUI *authUI;
+@property (nonatomic) FUIAuthUIDelegate *delegate;
+@end
+
+@implementation FIRAuthUITest
+
++ (void)initialize {
+  // An app needs to be configured before any instances of
+  // FIRAuth or FIRAuthUI can be created. [FIRApp configure]
+  // will crash unless there's a GoogleServices-Info.plist
+  // in the test target. Unfortunately, our .gitignore ignores
+  // those, so the plist has to be added manually on each clone :(
+  [FIRApp configure];
+}
+
+- (void)setUp {
+  [super setUp];
+  self.auth = [FIRAuth auth];
+  self.authUI = [FIRAuthUI defaultAuthUI];
+  self.delegate = [[FUIAuthUIDelegate alloc] init];
+}
+
+- (void)tearDown {
+  [super tearDown];
+}
+
+- (void)testItExists {
+  XCTAssert(self.auth != nil, @"expected default auth instance to exist");
+  XCTAssert(self.authUI != nil, @"expected default authUI instance to exist");
+}
+
+- (void)testItProducesAViewController {
+  self.authUI.delegate = self.delegate;
+  UIViewController *controller = [self.authUI authViewController];
+  XCTAssert(controller != nil, @"expected authUI to produce nonnull view controller");
+}
+
+@end
