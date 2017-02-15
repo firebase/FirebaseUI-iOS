d17f052264928b5d64e2eac1478a1997187ff0c9
Morgan Chen <morganchen12@gmail.com>
2017-02-15 11:10:40 -0800

    don't build objc sample in test script

--- test.sh
+++ test.sh
@@ -15,17 +15,20 @@ EXIT_STATUS=0
   CODE_SIGNING_REQUIRED=NO \
   | xcpretty) || EXIT_STATUS=$?
 
-cd samples/objc || EXIT_STATUS=$?
-pod install || EXIT_STATUS=$?
+# It'd be nice to test building the objc sample as a simple
+# integration test, but we don't have a GoogleService-Info.plist file
+# on Travis.
+# cd samples/objc
+# pod install
 
-(xcodebuild \
-  -workspace FirebaseUI-demo-objc.xcworkspace \
-  -scheme FirebaseUI-demo-objc \
-  -sdk iphonesimulator \
-  -destination 'platform=iOS Simulator,name=iPhone 7' \
-  build \
-  ONLY_ACTIVE_ARCH=YES \
-  CODE_SIGNING_REQUIRED=NO \
-  | xcpretty) || EXIT_STATUS=$?
+# (xcodebuild \
+#   -workspace FirebaseUI-demo-objc.xcworkspace \
+#   -scheme FirebaseUI-demo-objc \
+#   -sdk iphonesimulator \
+#   -destination 'platform=iOS Simulator,name=iPhone 7' \
+#   build \
+#   ONLY_ACTIVE_ARCH=YES \
+#   CODE_SIGNING_REQUIRED=NO \
+#   | xcpretty) || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
