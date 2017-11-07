8537d103e4a1856b87af8cb7f479607a77fa9c19
Ibrahim Ulukaya <ulukaya@gmail.com>
2017-11-07 16:52:11 -0500

    Update FUICustomAuthUIDelegate.swift

--- samples/swift/FirebaseUI-demo-swift/Samples/Auth/FUICustomAuthUIDelegate.swift
+++ samples/swift/FirebaseUI-demo-swift/Samples/Auth/FUICustomAuthUIDelegate.swift
@@ -21,18 +21,15 @@ import FirebaseAuth
 class FUICustomAuthDelegate: NSObject, FUIAuthDelegate {
 
   func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
-    guard let authError = error else { return }
-
-    let errorCode = UInt((authError as NSError).code)
-
-    switch errorCode {
-    case FUIAuthErrorCode.userCancelledSignIn.rawValue:
-      print("User cancelled sign-in");
-      break
-    default:
-      let detailedError = (authError as NSError).userInfo[NSUnderlyingErrorKey] ?? authError
-      print("Login error: \((detailedError as! NSError).localizedDescription)");
-    }
+    switch error {
+    case .some(let error as NSError) where UInt(error.code) == FUIAuthErrorCode.userCancelledSignIn.rawValue:
+      print("User cancelled sign-in")
+    case .some(let error as NSError) where error.userInfo[NSUnderlyingErrorKey] != nil:
+      print("Login error: \(error.userInfo[NSUnderlyingErrorKey]!)")
+    case .some(let error):
+      print("Login error: \(error.localizedDescription)")
+    case .none:
+      return
   }
 
   func authPickerViewController(forAuthUI authUI: FUIAuth) -> FUIAuthPickerViewController {
