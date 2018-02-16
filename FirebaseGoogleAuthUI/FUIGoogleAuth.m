eef57090082e8bea8dbc75e57b819b0e0fee1fe0
Zsika Phillip <zsika@google.com>
2018-02-16 01:01:49 -0800

    Facebook support for email_in_user_when_upgrading
    Adds support for Facebook when autolinking an exisiting user to a new federated user (Google).

--- FirebaseGoogleAuthUI/FUIGoogleAuth.m
+++ FirebaseGoogleAuthUI/FUIGoogleAuth.m
@@ -55,6 +55,11 @@ static NSString *const kSignInWithGoogle = @"SignInWithGoogle";
       @brief The callback which should be invoked when the sign in flow completes (or is cancelled.)
    */
   FIRAuthProviderSignInCompletionBlock _pendingSignInCallback;
+
+  /** @var _email
+      @brief The email address associated with this account.
+   */
+  NSString *_email;
 }
 
 - (instancetype)init {
@@ -140,6 +145,10 @@ static NSString *const kSignInWithGoogle = @"SignInWithGoogle";
   return [signIn handleURL:URL sourceApplication:sourceApplication annotation:nil];
 }
 
+- (NSString *)email {
+  return _email;
+}
+
 #pragma mark - GIDSignInDelegate methods
 
 - (void)signIn:(GIDSignIn *)signIn
@@ -158,6 +167,7 @@ static NSString *const kSignInWithGoogle = @"SignInWithGoogle";
     }
     return;
   }
+  _email = user.profile.email;
   UIActivityIndicatorView *activityView =
       [FUIAuthBaseViewController addActivityIndicator:_presentingViewController.view];
   [activityView startAnimating];
