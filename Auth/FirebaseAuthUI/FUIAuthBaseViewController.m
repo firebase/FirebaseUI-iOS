ed33aadb2cfef56f9f59d4f7e26bda8a3712f6a6
Xuan Zhang <xuaninbox@gmail.com>
2019-04-13 15:25:33 -0700

    Update FUIAuthBaseViewController.m

--- Auth/FirebaseAuthUI/FUIAuthBaseViewController.m
+++ Auth/FirebaseAuthUI/FUIAuthBaseViewController.m
@@ -22,21 +22,6 @@
 #import "FUIAuthUtils.h"
 #import "FUIAuth_Internal.h"
 
-/** @var kActivityIndiactorPadding
-    @brief The padding between the activity indiactor and its overlay.
- */
-static const CGFloat kActivityIndiactorPadding = 20.0f;
-
-/** @var kActivityIndiactorOverlayCornerRadius
-    @brief The corner radius of the overlay of the activity indicator.
- */
-static const CGFloat kActivityIndiactorOverlayCornerRadius = 20.0f;
-
-/** @var kActivityIndiactorOverlayOpacity
-    @brief The opacity of the overlay of the activity indicator.
- */
-static const CGFloat kActivityIndiactorOverlayOpacity = 0.8f;
-
 /** @var kActivityIndiactorAnimationDelay
     @brief The time delay before the activity indicator is actually animated.
  */
@@ -123,95 +108,7 @@ static NSString *const kAuthUICodingKey = @"authUI";
 }
 
 + (UIActivityIndicatorView *)addActivityIndicator:(UIView *)view {
-  if (!view) {
-    return nil;
-  }
-  UIActivityIndicatorView *activityIndicator =
-      [[UIActivityIndicatorView alloc]
-           initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
-  UIView *tintView = [[UIView alloc] initWithFrame:CGRectInset(activityIndicator.frame,
-                                                               -kActivityIndiactorPadding,
-                                                               -kActivityIndiactorPadding)];
-  tintView.backgroundColor =
-      [UIColor colorWithWhite:0 alpha:kActivityIndiactorOverlayOpacity];
-  tintView.layer.cornerRadius = kActivityIndiactorOverlayCornerRadius;
-  [activityIndicator addSubview:tintView];
-  
-  // Align tintView (transparent background).
-  tintView.translatesAutoresizingMaskIntoConstraints = NO;
-  [activityIndicator addConstraint:
-      [NSLayoutConstraint constraintWithItem:tintView
-                                   attribute:NSLayoutAttributeWidth
-                                   relatedBy:NSLayoutRelationEqual
-                                      toItem:nil
-                                   attribute:NSLayoutAttributeNotAnAttribute
-                                  multiplier:1
-                                    constant:CGRectGetWidth(tintView.frame)]];
-  [activityIndicator addConstraint:
-      [NSLayoutConstraint constraintWithItem:tintView
-                                   attribute:NSLayoutAttributeCenterX
-                                   relatedBy:NSLayoutRelationEqual
-                                      toItem:activityIndicator
-                                   attribute:NSLayoutAttributeCenterX
-                                  multiplier:1
-                                    constant:0]];
-
-  [activityIndicator addConstraint:
-      [NSLayoutConstraint constraintWithItem:tintView
-                                   attribute:NSLayoutAttributeHeight
-                                   relatedBy:NSLayoutRelationEqual
-                                      toItem:nil
-                                   attribute:NSLayoutAttributeNotAnAttribute
-                                  multiplier:1
-                                    constant:CGRectGetHeight(tintView.frame)]];
-  [activityIndicator addConstraint:
-      [NSLayoutConstraint constraintWithItem:tintView
-                                   attribute:NSLayoutAttributeCenterY
-                                   relatedBy:NSLayoutRelationEqual
-                                      toItem:activityIndicator
-                                   attribute:NSLayoutAttributeCenterY
-                                  multiplier:1
-                                    constant:0]];
-
-  [activityIndicator sendSubviewToBack:tintView];
-  
-  [view addSubview:activityIndicator];
-  // Align activity indicator.
-  activityIndicator.translatesAutoresizingMaskIntoConstraints = NO;
-  [view addConstraint:
-      [NSLayoutConstraint constraintWithItem:activityIndicator
-                                   attribute:NSLayoutAttributeWidth
-                                   relatedBy:NSLayoutRelationEqual
-                                      toItem:view
-                                   attribute:NSLayoutAttributeWidth
-                                  multiplier:1
-                                    constant:0]];
-  [view addConstraint:
-      [NSLayoutConstraint constraintWithItem:activityIndicator
-                                   attribute:NSLayoutAttributeCenterX
-                                   relatedBy:NSLayoutRelationEqual
-                                      toItem:view
-                                   attribute:NSLayoutAttributeCenterX
-                                  multiplier:1
-                                    constant:0]];
-
-  [view addConstraint:
-      [NSLayoutConstraint constraintWithItem:activityIndicator
-                                   attribute:NSLayoutAttributeHeight
-                                   relatedBy:NSLayoutRelationEqual
-                                      toItem:view
-                                   attribute:NSLayoutAttributeHeight
-                                  multiplier:1
-                                    constant:0]];
-  [view addConstraint:
-      [NSLayoutConstraint constraintWithItem:activityIndicator
-                                   attribute:NSLayoutAttributeCenterY
-                                   relatedBy:NSLayoutRelationEqual
-                                      toItem:view
-                                   attribute:NSLayoutAttributeCenterY
-                                  multiplier:1
-                                    constant:0]];
-  return activityIndicator;
+  return nil;
 }
 
 - (void)showAlertWithMessage:(NSString *)message {
