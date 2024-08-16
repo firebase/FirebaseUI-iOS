d9cb775ae44a3c2e1607b96ab02336af7d6de897
Paul Beusterien <paulbeusterien@google.com>
2024-08-16 08:17:59 -0700

    Fix Swift sample build

--- samples/swift/FirebaseUI-demo-swift/Samples/Auth/FUICustomPasswordVerificationViewController.swift
+++ samples/swift/FirebaseUI-demo-swift/Samples/Auth/FUICustomPasswordVerificationViewController.swift
@@ -15,6 +15,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 import FirebaseEmailAuthUI
 
 class FUICustomPasswordVerificationViewController: FUIPasswordVerificationViewController, UITextFieldDelegate {
