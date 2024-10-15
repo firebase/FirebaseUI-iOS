c211e1fd79687589ace08921d43bcee2062a98e9
Paul Beusterien <paulbeusterien@google.com>
2024-10-15 08:30:38 -0700

    Revert "Update FUICustomAuthUIDelegate.swift (#1205)"
    This reverts commit 04498aee234c753b4c05a0dd6625f241d8b55a52.

--- samples/swift/FirebaseUI-demo-swift/Samples/Auth/FUICustomAuthUIDelegate.swift
+++ samples/swift/FirebaseUI-demo-swift/Samples/Auth/FUICustomAuthUIDelegate.swift
@@ -20,7 +20,7 @@ import FirebaseEmailAuthUI
 
 class FUICustomAuthDelegate: NSObject, FUIAuthDelegate {
 
-  @objc func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+  func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
     switch error {
     case .some(let error as NSError) where UInt(error.code) == FUIAuthErrorCode.userCancelledSignIn.rawValue:
       print("User cancelled sign-in")
