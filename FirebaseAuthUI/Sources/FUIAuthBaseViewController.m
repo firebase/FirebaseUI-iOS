firebase/FirebaseUI-iOS

commit e3dbca53f4c920567e471c1abfc4594b819c6b5c

Various release updates (#1194)

FirebaseAuthUI/Sources/FUIAuthBaseViewController.m
@@ -131,7 +131,7 @@ static NSString *const kAuthUICodingKey = @"authUI";
   }
   UIActivityIndicatorView *activityIndicator =
       [[UIActivityIndicatorView alloc]
-           initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+       initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleLarge];
   UIView *tintView = [[UIView alloc] initWithFrame:CGRectInset(activityIndicator.frame,
                                                                -kActivityIndiactorPadding,
                                                                -kActivityIndiactorPadding)];

FirebaseAnonymousAuthUI.podspec
@@ -18,7 +18,7 @@ Pod::Spec.new do |s|
 
   s.public_header_files = 'FirebaseAnonymousAuthUI/Sources/Public/FirebaseAnonymousAuthUI/*.h'
   s.source_files = 'FirebaseAnonymousAuthUI/Sources/**/*.{h,m}'
-  s.dependency 'FirebaseAuthUI'
+  s.dependency 'FirebaseAuthUI', '>= 14.2'
   s.dependency 'FirebaseAuth', '>= 8.0', '< 12.0'
   s.dependency 'FirebaseCore'
   s.resource_bundles = {

FirebaseAuthUI/Sources/FUIAuthPickerViewController.m
@@ -171,14 +171,7 @@ static const CGFloat kTOSViewHorizontalMargin = 16.0f;
       + kButtonContainerBottomMargin + kTOSViewBottomMargin;
   CGFloat contentViewWidth = CGRectGetWidth(self.view.bounds);
   _scrollView.frame = self.view.frame;
-  CGFloat scrollViewHeight;
-  if (@available(iOS 11.0, *)) {
-    scrollViewHeight = CGRectGetHeight(_scrollView.frame) - _scrollView.safeAreaInsets.top;
-  } else {
-    scrollViewHeight = CGRectGetHeight(_scrollView.frame)
-        - CGRectGetHeight(self.navigationController.navigationBar.frame)
-        - CGRectGetHeight([UIApplication sharedApplication].statusBarFrame);
-  }
+  CGFloat scrollViewHeight = CGRectGetHeight(_scrollView.frame) - _scrollView.safeAreaInsets.top;
   CGFloat contentViewY = scrollViewHeight - contentViewHeight;
   if (contentViewY < 0) {
     contentViewY = 0;

FirebaseEmailAuthUI.podspec
@@ -20,7 +20,7 @@ Pod::Spec.new do |s|
   s.source_files = 'FirebaseEmailAuthUI/Sources/**/*.{h,m}'
   s.dependency 'FirebaseAuth'
   s.dependency 'FirebaseCore'
-  s.dependency 'FirebaseAuthUI'
+  s.dependency 'FirebaseAuthUI', '>= 14.2'
   s.dependency 'GoogleUtilities/UserDefaults'
   s.resource_bundles = {
     'FirebaseEmailAuthUI' => ['FirebaseEmailAuthUI/Sources/Resources/*.{xib,png}']

FirebaseFacebookAuthUI.podspec
@@ -22,7 +22,7 @@ Pod::Spec.new do |s|
   s.source_files = 'FirebaseFacebookAuthUI/Sources/**/*.{h,m}'
   s.dependency 'FirebaseAuth'
   s.dependency 'FirebaseCore'
-  s.dependency 'FirebaseAuthUI'
+  s.dependency 'FirebaseAuthUI', '>= 14.2'
   s.dependency 'FBSDKLoginKit', '>= 11.0', '< 17.0'
   s.dependency 'FBSDKCoreKit_Basics'
   s.resource_bundles = {

FirebaseGoogleAuthUI.podspec
@@ -20,7 +20,7 @@ Pod::Spec.new do |s|
   s.source_files = 'FirebaseGoogleAuthUI/Sources/**/*.{h,m}'
   s.dependency 'FirebaseAuth'
   s.dependency 'FirebaseCore'
-  s.dependency 'FirebaseAuthUI'
+  s.dependency 'FirebaseAuthUI', '>= 14.2'
   s.dependency 'GoogleSignIn', '~> 7.0'
   s.resource_bundles = {
     'FirebaseGoogleAuthUI' => ['FirebaseGoogleAuthUI/Sources/{Resources,Strings}/*.{png,lproj}']

FirebasePhoneAuthUI.podspec
@@ -19,7 +19,7 @@ Pod::Spec.new do |s|
   s.public_header_files = 'FirebasePhoneAuthUI/Sources/Public/FirebasePhoneAuthUI/*.h'
   s.source_files = 'FirebasePhoneAuthUI/Sources/**/*.{h,m}'
   s.dependency 'FirebaseAuth'
-  s.dependency 'FirebaseAuthUI'
+  s.dependency 'FirebaseAuthUI', '>= 14.2'
   s.resource_bundles = {
     'FirebasePhoneAuthUI' => ['FirebasePhoneAuthUI/Sources/{Resources,Strings}/*.{xib,json,lproj,png}']
   }

FirebaseUI.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = 'FirebaseUI'
-  s.version      = '14.1.0'
+  s.version      = '14.2.0'
   s.summary      = 'UI binding libraries for Firebase.'
   s.homepage     = 'https://github.com/firebase/FirebaseUI-iOS'
   s.license      = { :type => 'Apache 2.0', :file => 'LICENSE' }
@@ -19,43 +19,43 @@ Pod::Spec.new do |s|
   }
 
   s.subspec 'Database' do |database|
-    database.dependency 'FirebaseDatabaseUI', '~> 14.0'
+    database.dependency 'FirebaseDatabaseUI', '~> 14.2'
   end
 
   s.subspec 'Firestore' do |firestore|
-    firestore.dependency 'FirebaseFirestoreUI', '~> 14.0'
+    firestore.dependency 'FirebaseFirestoreUI', '~> 14.2'
   end
 
   s.subspec 'Storage' do |storage|
-    storage.dependency 'FirebaseStorageUI', '~> 14.0'
+    storage.dependency 'FirebaseStorageUI', '~> 14.2'
   end
 
   s.subspec 'Auth' do |auth|
-    auth.dependency 'FirebaseAuthUI', '~> 14.0'
+    auth.dependency 'FirebaseAuthUI', '~> 14.2'
   end
 
   s.subspec 'Anonymous' do |anonymous|
-    anonymous.dependency 'FirebaseAnonymousAuthUI', '~> 14.0'
+    anonymous.dependency 'FirebaseAnonymousAuthUI', '~> 14.2'
   end
 
   s.subspec 'Email' do |email|
-    email.dependency 'FirebaseEmailAuthUI', '~> 14.0'
+    email.dependency 'FirebaseEmailAuthUI', '~> 14.2'
   end
 
   s.subspec 'Facebook' do |facebook|
-    facebook.dependency 'FirebaseFacebookAuthUI', '~> 14.0'
+    facebook.dependency 'FirebaseFacebookAuthUI', '~> 14.2'
   end
 
   s.subspec 'Google' do |google|
-    google.dependency 'FirebaseGoogleAuthUI', '~> 14.0'
+    google.dependency 'FirebaseGoogleAuthUI', '~> 14.2'
   end
 
   s.subspec 'OAuth' do |oauth|
-    oauth.dependency 'FirebaseOAuthUI', '~> 14.0'
+    oauth.dependency 'FirebaseOAuthUI', '~> 14.2'
   end
 
   s.subspec 'Phone' do |phone|
-    phone.dependency 'FirebasePhoneAuthUI', '~> 14.0'
+    phone.dependency 'FirebasePhoneAuthUI', '~> 14.2'
   end
 
 end

commit 67dcb5717f1d910bdef21f92be8083750b05fb25

Set activity indicator color to white
Resolves https://github.com/firebase/FirebaseUI-iOS/issues/1207 caused by changes in https://github.com/firebase/FirebaseUI-iOS/pull/1194

FirebaseAuthUI/Sources/FUIAuthBaseViewController.m
@@ -132,6 +132,7 @@ static NSString *const kAuthUICodingKey = @"authUI";
   UIActivityIndicatorView *activityIndicator =
       [[UIActivityIndicatorView alloc]
        initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleLarge];
+  activityIndicator.color = [UIColor colorWithWhite:1 alpha:1];
   UIView *tintView = [[UIView alloc] initWithFrame:CGRectInset(activityIndicator.frame,
                                                                -kActivityIndiactorPadding,
                                                                -kActivityIndiactorPadding)];