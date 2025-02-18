f173131984e264ef061545b183a9c33285822634
russellwheatley <russellwheatley85@gmail.com>
2025-02-18 11:27:55 +0000

    feat: init SwiftUI library for FirebaseAuthSwiftUI

---
+++ FirebaseSwiftUI/FirebaseAuthSwiftUI/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FirebaseAuthSwiftUI",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "FirebaseAuthSwiftUI",
+            targets: ["FirebaseAuthSwiftUI"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "FirebaseAuthSwiftUI"),
+        .testTarget(
+            name: "FirebaseAuthSwiftUITests",
+            dependencies: ["FirebaseAuthSwiftUI"]
+        ),
+    ]
+)
