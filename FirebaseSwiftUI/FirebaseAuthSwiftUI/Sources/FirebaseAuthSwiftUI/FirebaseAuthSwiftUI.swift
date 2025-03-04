3f1d3b3938e3de59dfd96db03ca20c42d8c52e89
russellwheatley <russellwheatley85@gmail.com>
2025-03-04 13:55:09 +0000

    refactor: update implementation to more idiomatic Swift code

--- FirebaseSwiftUI/FirebaseAuthSwiftUI/Sources/FirebaseAuthSwiftUI/FirebaseAuthSwiftUI.swift
+++ FirebaseSwiftUI/FirebaseAuthSwiftUI/Sources/FirebaseAuthSwiftUI/FirebaseAuthSwiftUI.swift
@@ -2,101 +2,62 @@
 // https://docs.swift.org/swift-book
 import SwiftUI
 import FirebaseAuth
+import FirebaseCore
 import Combine
 
-
-public protocol AuthListenerProtocol {
-    func onError(_ error: Error)
-    func onBeforeSignIn()
-    func onSignedIn(_ user: User)
-    func onCanceled()
-    func onCredentialReceived(_ credential: AuthCredential)
-    func onCredentialLinked(_ credential: AuthCredential)
-    // TODO - add when I get to this point
-    // func onMFARequired(_ resolver: MultiFactorResolver)
+public protocol FUIAuthProvider {
+  var providerId: String { get }
 }
 
-public protocol AuthProviderProtocol {
-    associatedtype Listener: AuthListenerProtocol
-    associatedtype Credential: AuthCredential
-    
-    var auth: Auth { get }
-    var authListener: Listener { get set }
-    var providerId: String { get }
-    
-    func signInWithCredential(_ credential: Credential)
-    func linkWithCredential(_ credential: Credential)
-}
+public class FirebaseAuthSwiftUI {
+  private var auth: Auth
+  private var authProviders: [FUIAuthProvider] = []
 
-public enum AuthAction {
-    /// Performs user sign in
-    case signIn
-
-    /// Creates a new account with for a provided credential
-    case signUp
-
-    /// Links a provided credential with currently signed in user account
-    case link
-
-    /// Disables automatic credential handling.
-    /// It's up to the user to decide what to do with the obtained credential.
-    case none
-}
-
-open class AuthProvider<Listener: AuthListenerProtocol>: AuthProviderProtocol {
-  public var auth: Auth = Auth.auth()
-  public var authListener: Listener
-  public var providerId: String
-    
-  public init(listener: Listener, providerId: String) {
-        self.authListener = listener
-        self.providerId = providerId
-    }
-  
-  var shouldUpgradeAnonymous: Bool {
-      return Auth.auth().currentUser?.isAnonymous ?? false
+  public init(auth: Auth? = nil) {
+      // Use the provided Auth instance or default
+      self.auth = auth ?? Auth.auth()
   }
+
+  public func authProviders(providers: [FUIAuthProvider]) {
+      self.authProviders = providers
+  }
+}
+
+// main auth view - can be composed of custom views or fallback to default views. We can also pass state upwards as opposed to callback.
+// Negates the need for a delegate used in UIKit
+public struct FUIAuthView: View {
+  private var FUIAuth: FirebaseAuthSwiftUI
+  private var authPickerView: any AuthPickerView
     
-  public func signInWithCredential(_ credential: AuthCredential) {
-        authListener.onBeforeSignIn()
-        auth.signIn(with: credential) { [weak self] result, error in
-            if let error = error {
-                self?.authListener.onError(error)
-            } else if let user = result?.user {
-                self?.authListener.onSignedIn(user)
-            }
-        }
-    }
-    
-  public func linkWithCredential(_ credential: AuthCredential) {
-        authListener.onCredentialReceived(credential)
-        guard let user = auth.currentUser else { return }
-        
-        user.link(with: credential) { [weak self] result, error in
-            if let error = error {
-                self?.authListener.onError(error)
-            } else {
-                self?.authListener.onCredentialLinked(credential)
-            }
-        }
-    }
+
+  public init(FUIAuth: FirebaseAuthSwiftUI,_authPickerView: some AuthPickerView = FUIAuthPicker()) {
+    self.FUIAuth = FUIAuth
+    self.authPickerView = _authPickerView
+
+  }
   
-  func onCredentialReceived(credential: AuthCredential, action: AuthAction) {
-      switch action {
-      case .link:
-          linkWithCredential(credential)
-      case .signIn, .signUp:
-          // Only email provider has a different action for sign in and sign up
-          // and implements its own sign up logic.
-          if shouldUpgradeAnonymous {
-              linkWithCredential(credential)
-          } else {
-              signInWithCredential(credential)
-          }
-      case .none:
-          authListener.onCredentialReceived(credential)
+    public var body: some View {
+      VStack {
+        AnyView(authPickerView)
       }
-  }
-    // TODO - fetchSignInMethodsForEmail/fetchProvidersForEmail is deprecated
+    }
 }
 
+
+public protocol AuthPickerView: View {
+    var title: String { get }
+}
+
+public struct FUIAuthPicker: AuthPickerView {
+  public init(title: String? = nil) {
+    self.title = title ?? "Auth Picker View"
+  }
+    public var title: String = "Main View"
+    public var body: some View {
+        VStack {
+            Text(title)
+                .font(.largeTitle)
+                .padding()
+        }
+    }
+}
--- FirebaseSwiftUI/FirebaseEmailAuthSwiftUI/Sources/FirebaseEmailAuthSwiftUI/FirebaseEmailAuthSwiftUI.swift
+++ FirebaseSwiftUI/FirebaseEmailAuthSwiftUI/Sources/FirebaseEmailAuthSwiftUI/FirebaseEmailAuthSwiftUI.swift
@@ -4,21 +4,9 @@
 import FirebaseAuthSwiftUI
 import FirebaseAuth
 
-class EmailAuthProvider<Listener: AuthListenerProtocol>: AuthProvider<Listener> {
-   init(listener: Listener) {
-      super.init(listener: listener, providerId: "password")
-   }
-  
-  func signUpWithEmailAndPassword(email: String, password: String) {
-    self.authListener.onBeforeSignIn()
-    Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-            if let error = error {
-                // Handle error
-              self.authListener.onError(error)
-            } else if let authResult = authResult {
-                // Notify listener on successful sign-in
-              self.authListener.onSignedIn(authResult.user)
-            }
-        }
+class EmailAuthProvider: FUIAuthProvider {
+  var providerId: String {
+      return "password"
   }
+  
 }
