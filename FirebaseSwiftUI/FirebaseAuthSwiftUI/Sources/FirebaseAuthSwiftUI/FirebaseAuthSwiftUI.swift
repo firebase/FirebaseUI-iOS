f0a381134eab945680d833eb5ca3aea861b60c27
russellwheatley <russellwheatley85@gmail.com>
2025-03-04 13:57:39 +0000

    chore: code comment

--- FirebaseSwiftUI/FirebaseAuthSwiftUI/Sources/FirebaseAuthSwiftUI/FirebaseAuthSwiftUI.swift
+++ FirebaseSwiftUI/FirebaseAuthSwiftUI/Sources/FirebaseAuthSwiftUI/FirebaseAuthSwiftUI.swift
@@ -23,7 +23,7 @@ public class FirebaseAuthSwiftUI {
   }
 }
 
-// main auth view - can be composed of custom views or fallback to default views. We can also pass state upwards as opposed to callback.
+// main auth view - can be composed of custom views or fallback to default views. We can also pass state upwards as opposed to having callbacks.
 // Negates the need for a delegate used in UIKit
 public struct FUIAuthView: View {
   private var FUIAuth: FirebaseAuthSwiftUI
