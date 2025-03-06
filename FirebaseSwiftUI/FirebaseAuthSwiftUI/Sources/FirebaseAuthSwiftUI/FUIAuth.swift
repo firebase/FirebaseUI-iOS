27ba4d7403e7f50aa41504d4e1db8405272011e2
russellwheatley <russellwheatley85@gmail.com>
2025-03-06 16:34:40 +0000

    configureable warning view

--- FirebaseSwiftUI/FirebaseAuthSwiftUI/Sources/FirebaseAuthSwiftUI/FUIAuth.swift
+++ FirebaseSwiftUI/FirebaseAuthSwiftUI/Sources/FirebaseAuthSwiftUI/FUIAuth.swift
@@ -205,39 +205,53 @@ public struct EmailEntryView: View {
     if !AuthUtils.isValidEmail(email) {
       invalidEmailWarning = true
     }
-
-    // TODO-
-    // 2. Create another view/alert which renders if email isn't valid
   }
 }
 
+public struct WarningViewConfiguration {
+  public var messageFont: Font = .headline
+  public var buttonFont: Font = .body
+  public var buttonBackgroundColor: Color = .blue
+  public var buttonForegroundColor: Color = .white
+  public var buttonCornerRadius: CGFloat = 8
+  public var viewBackgroundColor: Color = .white
+  public var viewCornerRadius: CGFloat = 12
+  public var shadowRadius: CGFloat = 10
+  public var strokeColor: Color = .gray
+  public var strokeLineWidth: CGFloat = 1
+  public var frameWidth: CGFloat = 300
+  public var frameHeight: CGFloat = 150
+}
+
+// TODO: - figure out a way to make this configurable. Tough because we need to use binding. Could be class with certain attributes rather than a view
 public struct WarningView: View {
   @Binding var invalidEmailWarning: Bool
   var message: String
+  var configuration: WarningViewConfiguration
 
   public var body: some View {
     VStack {
       Text(message)
-        .font(.headline)
+        .font(configuration.messageFont)
         .padding()
       Button(action: {
         invalidEmailWarning = false
       }) {
         Text("OK")
-          .font(.body)
+          .font(configuration.buttonFont)
           .padding()
-          .background(Color.blue)
-          .foregroundColor(.white)
-          .cornerRadius(8)
+          .background(configuration.buttonBackgroundColor)
+          .foregroundColor(configuration.buttonForegroundColor)
+          .cornerRadius(configuration.buttonCornerRadius)
       }
     }
-    .frame(width: 300, height: 150)
-    .background(Color.white)
-    .cornerRadius(12)
-    .shadow(radius: 10)
+    .frame(width: configuration.frameWidth, height: configuration.frameHeight)
+    .background(configuration.viewBackgroundColor)
+    .cornerRadius(configuration.viewCornerRadius)
+    .shadow(radius: configuration.shadowRadius)
     .overlay(
-      RoundedRectangle(cornerRadius: 12)
-        .stroke(Color.gray, lineWidth: 1)
+      RoundedRectangle(cornerRadius: configuration.viewCornerRadius)
+        .stroke(configuration.strokeColor, lineWidth: configuration.strokeLineWidth)
     )
     .padding()
   }
