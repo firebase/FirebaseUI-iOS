b257f403e6e32d28e2f46c12843bbe5ffa67befc
russellwheatley <russellwheatley85@gmail.com>
2025-03-06 11:41:14 +0000

    clean up db setup in example app

--- samples/swiftui/FirebaseSwiftUIExample/FirebaseSwiftUIExample/FirebaseSwiftUIExampleApp.swift
+++ samples/swiftui/FirebaseSwiftUIExample/FirebaseSwiftUIExample/FirebaseSwiftUIExampleApp.swift
@@ -17,19 +17,6 @@ struct FirebaseSwiftUIExampleApp: App {
     FirebaseApp.configure()
   }
 
-  var sharedModelContainer: ModelContainer = {
-    let schema = Schema([
-      Item.self,
-    ])
-    let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-    do {
-      return try ModelContainer(for: schema, configurations: [modelConfiguration])
-    } catch {
-      fatalError("Could not create ModelContainer: \(error)")
-    }
-  }()
-
   var body: some Scene {
     WindowGroup {
       // Put this at top level so user can control it in their app
@@ -38,6 +25,5 @@ struct FirebaseSwiftUIExampleApp: App {
         FUIAuthView(FUIAuth: firebaseAuthUI)
       }
     }
-    .modelContainer(sharedModelContainer)
   }
 }
