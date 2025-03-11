81174147235144754bbafdd6143c0fa55252d48d
russellwheatley <russellwheatley85@gmail.com>
2025-03-11 07:36:55 +0000

    fix: stop breaking view identity

--- FirebaseSwiftUI/FirebaseEmailAuthSwiftUI/Sources/FirebaseEmailAuthSwiftUI/Views/EmailEntry.swift
+++ FirebaseSwiftUI/FirebaseEmailAuthSwiftUI/Sources/FirebaseEmailAuthSwiftUI/Views/EmailEntry.swift
@@ -7,33 +7,31 @@ public struct EmailEntryView: View {
   @EnvironmentObject var authFUI: FUIAuth
 
   public var body: some View {
-    if invalidEmailWarning {
-      WarningView(
-        invalidEmailWarning: $invalidEmailWarning
-      )
-    } else {
-      VStack {
-        Text("Email")
-          .font(.largeTitle)
-          .padding()
-        TextField("Email", text: $email, onCommit: emailSubmit)
-          .textFieldStyle(RoundedBorderTextFieldStyle())
-          .padding()
-      }
-      .padding(20)
-      .background(Color.white)
-      .cornerRadius(12)
-      .shadow(radius: 10)
-      .padding()
-      // TODO: - figure out why this is causing exception: Ambiguous use of 'toolbar(content:)'
-      //    .toolbar {
-      //      ToolbarItemGroup(placement: .navigationBarTrailing) {
-      //        Button("Next") {
-      //          handleNext()
-      //        }
-      //      }
-      //    }
+    WarningView(
+      invalidEmailWarning: $invalidEmailWarning
+    ).disabled(invalidEmailWarning == true)
+
+    VStack {
+      Text("Email")
+        .font(.largeTitle)
+        .padding()
+      TextField("Email", text: $email, onCommit: emailSubmit)
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+        .padding()
     }
+    .padding(20)
+    .background(Color.white)
+    .cornerRadius(12)
+    .shadow(radius: 10)
+    .disabled(invalidEmailWarning == false)
+    // TODO: - figure out why this is causing exception: Ambiguous use of 'toolbar(content:)'
+//      .toolbar {
+//        ToolbarItemGroup(placement: .navigationBarTrailing) {
+//          Button("Next") {
+//            emailSubmit()
+//          }
+//        }
+//      }
   }
 
   private func emailSubmit() {
