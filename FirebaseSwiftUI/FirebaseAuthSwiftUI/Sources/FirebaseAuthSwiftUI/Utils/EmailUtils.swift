526e7602dc83c775f3e5b308ef6e2c70d8084d96
russellwheatley <russellwheatley85@gmail.com>
2025-03-21 11:26:18 +0000

    chore: rm email package, moving to core

---
+++ FirebaseSwiftUI/FirebaseAuthSwiftUI/Sources/FirebaseAuthSwiftUI/Utils/EmailUtils.swift
@@ -0,0 +1,8 @@
+class EmailUtils {
+  static let emailRegex = ".+@([a-zA-Z0-9\\-]+\\.)+[a-zA-Z0-9]{2,63}"
+
+  static func isValidEmail(_ email: String) -> Bool {
+    let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+    return emailPredicate.evaluate(with: email)
+  }
+}
