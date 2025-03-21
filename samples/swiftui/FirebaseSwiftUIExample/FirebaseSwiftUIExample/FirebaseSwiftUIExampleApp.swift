3ef9459da4411eecfb1ed56b2131b2551780fed4
russellwheatley <russellwheatley85@gmail.com>
2025-03-21 12:28:40 +0000

    chore: fix up example app

--- samples/swiftui/FirebaseSwiftUIExample/FirebaseSwiftUIExample/FirebaseSwiftUIExampleApp.swift
+++ samples/swiftui/FirebaseSwiftUIExample/FirebaseSwiftUIExample/FirebaseSwiftUIExampleApp.swift
@@ -8,45 +8,19 @@
 import FirebaseAuth
 import FirebaseAuthSwiftUI
 import FirebaseCore
-import FirebaseEmailAuthSwiftUI
 import SwiftData
 import SwiftUI
 
-struct CustomTextModifier: ViewModifier {
-  func body(content: Content) -> some View {
-    content
-      .font(.headline)
-      .background(.red)
-  }
-}
-
 @main
 struct FirebaseSwiftUIExampleApp: App {
-  var authUI: FUIAuth
-
   init() {
     FirebaseApp.configure()
-    let firebaseAuthUI = FUIAuth()
-    authUI = firebaseAuthUI
-//    authUI.authProviders(providers: [EmailAuthProvider()])
   }
 
   var body: some Scene {
     WindowGroup {
-      // Put this at top level so user can control it in their app
       NavigationView {
-        FUIAuthView(
-          FUIAuth: authUI,
-          // method 1 of setting view modifier
-          authPickerView: AuthPickerView(title: "Custom Auth Picker", textModifier: { Text in
-            Text.bold()
-          }) {
-            VStack {
-              // method 2 of setting view modifier
-              EmailAuth<DefaultEmailAuthButtonStyle, DefaultEmailButtonTextStyle>()
-            }
-          }
-        )
+        AuthenticationScreen().environment(AuthEnvironment.shared)
       }
     }
   }
