7d7a03981874e5efc4272abea842bb1cb0983a60
russellwheatley <russellwheatley85@gmail.com>
2025-03-24 17:42:32 +0000

    update email provider to create user

--- FirebaseSwiftUI/FirebaseAuthSwiftUI/Sources/FirebaseAuthSwiftUI/Services/EmailProvider.swift
+++ FirebaseSwiftUI/FirebaseAuthSwiftUI/Sources/FirebaseAuthSwiftUI/Services/EmailProvider.swift
@@ -10,14 +10,8 @@ public class EmailAuthProvider {
   }
 
   func signIn(withEmail email: String, password: String) async throws {
-    authEnvironment.authenticationState = .authenticating
-    do {
-      try await authEnvironment.auth.signIn(withEmail: email, password: password)
-      authEnvironment.updateAuthenticationState()
-    } catch {
-      authEnvironment.authenticationState = .unauthenticated
-      throw error
-    }
+    let credential = FirebaseAuth.EmailAuthProvider.credential(withEmail: email, password: password)
+    try await authEnvironment.signIn(with: credential)
   }
 
   func createUser(withEmail email: String, password: String) async throws {
