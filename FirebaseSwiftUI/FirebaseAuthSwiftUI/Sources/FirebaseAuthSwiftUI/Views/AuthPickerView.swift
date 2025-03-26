a05cf8696d2e27bc4de1cfcab6172f30287b409b
russellwheatley <russellwheatley85@gmail.com>
2025-03-26 11:47:19 +0000

    implement AuthPickerView logic for sign-up & login

--- FirebaseSwiftUI/FirebaseAuthSwiftUI/Sources/FirebaseAuthSwiftUI/Views/AuthPickerView.swift
+++ FirebaseSwiftUI/FirebaseAuthSwiftUI/Sources/FirebaseAuthSwiftUI/Views/AuthPickerView.swift
@@ -8,12 +8,32 @@ public struct AuthPickerView<Content: View>: View {
     self.providerButtons = providerButtons
   }
 
+  private func switchFlow() {
+    authEnvironment.authenticationFlow = authEnvironment
+      .authenticationFlow == .login ? .signUp : .login
+  }
+
   public var body: some View {
     VStack {
       if authEnvironment.authenticationState == .authenticated {
         SignedInView()
       } else {
+        Text(authEnvironment.authenticationFlow == .login ? "Login" : "Sign up")
         providerButtons()
+        HStack {
+          Text(authEnvironment
+            .authenticationFlow == .login ? "Don't have an account yet?" :
+            "Already have an account?")
+          Button(action: {
+            withAnimation {
+              switchFlow()
+            }
+          }) {
+            Text(authEnvironment.authenticationFlow == .signUp ? "Log in" : "Sign up")
+              .fontWeight(.semibold)
+              .foregroundColor(.blue)
+          }
+        }
       }
     }
   }
