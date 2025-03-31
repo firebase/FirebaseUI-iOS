68b2c0057a7d46e690032363db49272415b48733
russellwheatley <russellwheatley85@gmail.com>
2025-03-31 16:26:16 +0100

    chore: rm Email auth as separate package

--- FirebaseSwiftUI/FirebaseEmailAuthSwiftUI/Tests/FirebaseEmailAuthSwiftUITests/FirebaseEmailAuthSwiftUITests.swift
+++
@@ -1,6 +0,0 @@
-@testable import FirebaseEmailAuthSwiftUI
-import Testing
-
-@Test func example() async throws {
-  // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
