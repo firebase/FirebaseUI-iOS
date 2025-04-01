e922d7406f9e29cc379e593ee4a8bc57f31465d1
russellwheatley <russellwheatley85@gmail.com>
2025-04-01 14:32:40 +0100

    google auth provider formatting

--- FirebaseSwiftUI/FirebaseGoogleSwiftUI/Sources/Services/GoogleProviderSwift.swift
+++ FirebaseSwiftUI/FirebaseGoogleSwiftUI/Sources/Services/GoogleProviderSwift.swift
@@ -13,11 +13,11 @@ public class GoogleProviderSwift: @preconcurrency GoogleProviderProtocol {
   public init(scopes: [String]? = nil) {
     self.scopes = scopes ?? kDefaultScopes
   }
-
+  
   public func handleUrl(_ url: URL) -> Bool {
     return GIDSignIn.sharedInstance.handle(url)
   }
-
+  
   @MainActor public func signInWithGoogle(clientID: String) async throws -> AuthCredential {
     guard let presentingViewController = (UIApplication.shared.connectedScenes
       .first as? UIWindowScene)?.windows.first?.rootViewController else {
@@ -29,34 +29,35 @@ public class GoogleProviderSwift: @preconcurrency GoogleProviderProtocol {
         ]
       )
     }
-
+    
     let config = GIDConfiguration(clientID: clientID)
     GIDSignIn.sharedInstance.configuration = config
     
     return try await withCheckedThrowingContinuation { continuation in
-        GIDSignIn.sharedInstance.signIn(
-          withPresenting: presentingViewController
-        ) { result, error in
-          if let error = error {
-            continuation.resume(throwing: error)
-            return
-          }
-
-          guard let user = result?.user,
-                let idToken = user.idToken?.tokenString else {
-            continuation.resume(throwing: NSError(
-              domain: "GoogleProviderSwiftErrorDomain",
-              code: 2,
-              userInfo: [
-                NSLocalizedDescriptionKey: "Failed to retrieve user or idToken.",
-              ]
-            ))
-            return
-          }
-
-          let credential = GoogleAuthProvider.credential(withIDToken: idToken,
-                                                         accessToken: user.accessToken.tokenString)
-          continuation.resume(returning: credential)
+      GIDSignIn.sharedInstance.signIn(
+        withPresenting: presentingViewController
+      ) { result, error in
+        if let error = error {
+          continuation.resume(throwing: error)
+          return
         }
+        
+        guard let user = result?.user,
+              let idToken = user.idToken?.tokenString else {
+          continuation.resume(throwing: NSError(
+            domain: "GoogleProviderSwiftErrorDomain",
+            code: 2,
+            userInfo: [
+              NSLocalizedDescriptionKey: "Failed to retrieve user or idToken.",
+            ]
+          ))
+          return
+        }
+        
+        let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                       accessToken: user.accessToken.tokenString)
+        continuation.resume(returning: credential)
+      }
+    }
   }
 }
