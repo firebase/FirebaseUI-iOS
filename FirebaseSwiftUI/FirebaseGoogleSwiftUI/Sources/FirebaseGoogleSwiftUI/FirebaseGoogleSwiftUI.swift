aa8268aa54db44d14f5bd6943c4a8cf8e14106f4
russellwheatley <russellwheatley85@gmail.com>
2025-04-01 10:24:01 +0100

    google provider - init tweak

--- FirebaseSwiftUI/FirebaseGoogleSwiftUI/Sources/FirebaseGoogleSwiftUI/FirebaseGoogleSwiftUI.swift
+++ FirebaseSwiftUI/FirebaseGoogleSwiftUI/Sources/FirebaseGoogleSwiftUI/FirebaseGoogleSwiftUI.swift
@@ -2,8 +2,15 @@ import FirebaseAuthSwiftUI
 
 let kGoogleUserInfoEmailScope = "https://www.googleapis.com/auth/userinfo.email"
 let kGoogleUserInfoProfileScope = "https://www.googleapis.com/auth/userinfo.profile"
+let kDefaultScopes = [kGoogleUserInfoEmailScope, kGoogleUserInfoProfileScope]
 
 public class GoogleProviderSwift: GoogleProviderProtocol {
   let scopes: [String]
-  public init(scopes _: [String] = [kGoogleUserInfoEmailScope, kGoogleUserInfoProfileScope]) {}
+  let shortName = "Google"
+  let providerId = "google.com"
+  public init(scopes _: [String] = kDefaultScopes) {
+    self.scopes = scopes
+  }
+  
+  
 }
