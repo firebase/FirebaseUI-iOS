eed57b8355edab1af0a3b58ef6c22d34a8711867
russellwheatley <russellwheatley85@gmail.com>
2025-05-08 13:03:52 +0100

    chore: rm var constants - not needed

--- FirebaseSwiftUI/FirebaseAuthSwiftUI/Sources/Utils/StringUtils.swift
+++ FirebaseSwiftUI/FirebaseAuthSwiftUI/Sources/Utils/StringUtils.swift
@@ -3,16 +3,6 @@ import SwiftUI
 
 let kKeyNotFound = "Key not found"
 
-// Errors
-let kUsersNotFoundError = "UserNotFoundError"
-let kEmailAlreadyInUseError = "EmailAlreadyInUseError"
-let kInvalidEmailError = "InvalidEmailError"
-let kWeakPasswordError = "WeakPasswordError"
-let kSignUpTooManyTimesError = "SignUpTooManyTimesError"
-let kWrongPasswordError = "WrongPasswordError"
-let kAccountDisabledError = "AccountDisabledError"
-let kEmailsDoNotMatchError = "EmailsDoNotMatchError"
-let kUnknownError = "UnknownError"
 
 public class StringUtils {
   let bundle: Bundle
@@ -27,37 +17,37 @@ public class StringUtils {
   }
 
   public func localizedErrorMessage(for error: Error) -> String {
-    let authError = error as NSError
-    let errorCode = AuthErrorCode(rawValue: authError.code)
-    switch errorCode {
-    case .emailAlreadyInUse:
-      return localizedString(
-        for: kEmailAlreadyInUseError
-      )
-    case .invalidEmail:
-      return localizedString(for: kInvalidEmailError)
-    case .weakPassword:
-      return localizedString(for: kWeakPasswordError)
-    case .tooManyRequests:
-      return localizedString(
-        for: kSignUpTooManyTimesError
-      )
-    case .wrongPassword:
-      return localizedString(
-        for: kWrongPasswordError
-      )
-    case .userNotFound:
-      return localizedString(
-        for: kUsersNotFoundError
-      )
-    case .userDisabled:
-      return localizedString(
-        for: kAccountDisabledError
-      )
-    default:
-      return error.localizedDescription
+      let authError = error as NSError
+      let errorCode = AuthErrorCode(rawValue: authError.code)
+      switch errorCode {
+      case .emailAlreadyInUse:
+        return localizedString(
+          for: "EmailAlreadyInUseError"
+        )
+      case .invalidEmail:
+        return localizedString(for: "InvalidEmailError")
+      case .weakPassword:
+        return localizedString(for: "WeakPasswordError")
+      case .tooManyRequests:
+        return localizedString(
+          for: "SignUpTooManyTimesError"
+        )
+      case .wrongPassword:
+        return localizedString(
+          for: "WrongPasswordError"
+        )
+      case .userNotFound:
+        return localizedString(
+          for: "UserNotFoundError"
+        )
+      case .userDisabled:
+        return localizedString(
+          for: "AccountDisabledError"
+        )
+      default:
+        return error.localizedDescription
+      }
     }
-  }
 
   /// Auth Picker title
   /// found in:
