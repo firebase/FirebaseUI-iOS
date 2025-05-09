40ae462df7017f9e3c6802141e84424084f0aca3
russellwheatley <russellwheatley85@gmail.com>
2025-05-09 09:08:31 +0100

    rm facebook provider from init

--- samples/swiftui/FirebaseSwiftUIExample/FirebaseSwiftUIExample/ContentView.swift
+++ samples/swiftui/FirebaseSwiftUIExample/FirebaseSwiftUIExample/ContentView.swift
@@ -31,7 +31,6 @@ struct ContentView: View {
     let phoneAuthProvider = PhoneAuthProviderAuthUI()
     authService = AuthService(
       configuration: configuration,
-      facebookProvider: facebookProvider,
       phoneAuthProvider: phoneAuthProvider
     )
     .withGoogleSignIn()
