a657fe1293429f5fb2e793f87d710204255fa263
russellwheatley <russellwheatley85@gmail.com>
2025-05-16 11:25:07 +0100

    test: createUser/sign in with email/password

--- samples/swiftui/FirebaseSwiftUIExample/FirebaseSwiftUIExampleTests/FirebaseSwiftUIExampleTests.swift
+++ samples/swiftui/FirebaseSwiftUIExample/FirebaseSwiftUIExampleTests/FirebaseSwiftUIExampleTests.swift
@@ -4,12 +4,61 @@
 //
 //  Created by Russell Wheatley on 18/02/2025.
 //
-
+import FirebaseAuth
+import FirebaseAuthSwiftUI
+import FirebaseCore
 @testable import FirebaseSwiftUIExample
 import Testing
 
+let kEmailAddress = "test@example.com"
+let kPassword = "123456"
+
 struct FirebaseSwiftUIExampleTests {
-  @Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  @MainActor
+  func prepareFreshAuthService() async throws -> AuthService {
+    configureFirebaseIfNeeded()
+    try await clearAuthEmulatorState()
+
+    let auth = Auth.auth()
+    return AuthService(auth: auth)
+  }
+
+  @Test
+  @MainActor
+  func testCreateEmailPasswordUser() async throws {
+    let service = try await prepareFreshAuthService()
+
+    #expect(service.authenticationState == .unauthenticated)
+    #expect(service.authView == .authPicker)
+    #expect(service.errorMessage.isEmpty)
+    #expect(service.signedInCredential == nil)
+    try await service.createUser(withEmail: kEmailAddress, password: kPassword)
+
+    #expect(service.authenticationState == .authenticated)
+    #expect(service.authView == .authPicker)
+    #expect(service.errorMessage.isEmpty)
+    // TODO: - reinstate once this PR is merged: https://github.com/firebase/FirebaseUI-iOS/pull/1256
+//    #expect(service.signedInCredential is AuthCredential)
+  }
+
+  @Test
+  @MainActor
+  func testSignInUser() async throws {
+    let service = try await prepareFreshAuthService()
+    // TODO: - it clears emulator data but seems like it is still there?
+//    try await service.createUser(withEmail: kEmailAddress, password: kPassword)
+//    try await service.signOut()
+//    #expect(service.authenticationState == .unauthenticated)
+//    #expect(service.authView == .authPicker)
+//    #expect(service.errorMessage.isEmpty)
+//    #expect(service.signedInCredential == nil)
+
+    try await service.signIn(withEmail: kEmailAddress, password: kPassword)
+
+    #expect(service.authenticationState == .authenticated)
+    #expect(service.authView == .authPicker)
+    #expect(service.errorMessage.isEmpty)
+    // TODO: - reinstate once this PR is merged: https://github.com/firebase/FirebaseUI-iOS/pull/1256
+//    #expect(service.signedInCredential is AuthCredential)
   }
 }
