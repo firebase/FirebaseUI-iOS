55f95b8d168421a56d4f74e3ebe9914092226c6b
russellwheatley <russellwheatley85@gmail.com>
2025-10-03 12:43:09 +0100

    test: MFA enrolment tests

---
+++ samples/swiftui/FirebaseSwiftUIExample/FirebaseSwiftUIExampleUITests/TestUtils.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+func createEmail() -> String {
+  let before = UUID().uuidString.prefix(8)
+  let after = UUID().uuidString.prefix(6)
+  return "\(before)@\(after).com"
+}
