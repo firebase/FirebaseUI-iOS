432c0eaa8d928d2d2feb919521a2180bb4910a37
Ademola Fadumo <demolafadumo@gmail.com>
2025-11-07 12:36:23 +0100

    move existing sample app to e2eTest folder and create new sample app

---
+++ samples/swiftui/FirebaseUIApp/FirebaseUIApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseUIApp
+//
+//  Created by Ademola Fadumo on 07/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ samples/swiftui/FirebaseUIApp/FirebaseUIApp/FirebaseUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseUIAppApp.swift
+//  FirebaseUIApp
+//
+//  Created by Ademola Fadumo on 07/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
